0d7bd24bef2cc4bd9195fe79f3832e7e95bbe14d
Alixd911 <alixdonkers23@gmail.com>
2023-03-30 10:44:39 +0200

    Bugs fixed

--- websitescrape.py
+++ websitescrape.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
-import urllib3
 import urllib.request
 import urllib.parse
-import pandas
-import logging
 import ssl
 import json
 import discord
+import datetime
+import asyncio
+import pytz
+
  
 
 class ComplexEncoder(json.JSONEncoder):
@@ -18,10 +19,6 @@ class PyEcoRoot:
         self.currency = currency
         self.eco_element = eco_element
 
-import json
-import urllib.request
-import ssl
-from bs4 import BeautifulSoup
 
 class PyEcoElement:
     def __init__(self, currency, event, impact, time_eastern):
@@ -46,6 +43,7 @@ class PyEcoCal:
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         
         urleco = baseURL + date
+        print(f"Scraping {baseURL}...")
 
         try:
             # Open the URL using the opener
@@ -59,28 +57,29 @@ class PyEcoCal:
         table = soup.find_all("tr", class_="calendar_row")
 
         ecoday = []
+        time_temp = ""
         for item in table:
-            impact = item.find_all("td", {"class":"calendar__impact"})
-            if impact:
-                dict_ = {}
-                dict_["Currency"] = item.find_all("td", {"class":"calendar__currency"})[0].text.strip() #Currency
-                dict_["Event"] = item.find_all("td",{"class":"calendar__event"})[0].text.strip() #Event Name
-                dict_["Time_Eastern"] = item.find_all("td", {"class":"calendar__time"})[0].text #Time Eastern
-                
-            for icon in impact:
-                spans = icon.find_all("span")
-                if spans:
-                    impact_value = spans[0]['title'].split(' ', 1)[0]
-                    if impact_value == "High":
-                        dict_["Impact"] = impact_value
-                    else:
-                        print('hoi')
-                else:
-                    print("No spans found for icon:", icon)
-                    break
+            dict_ = {}
 
-                if "Impact" in dict_:
-                    ecoday.append(dict_)
+            time_eastern = item.find_all("td", {"class":"calendar__time"})
+            for time_item in time_eastern:
+                time_item = time_item.text.strip()
+                if time_item != "": # only save values that have text, otherwise dont replace the value
+                  time_temp = time_item
+
+                impact = item.find_all("td", {"class":"calendar__impact"})
+                for icon in impact:
+                    spans = icon.find_all("span")
+                    if spans:
+                        impact_value = spans[0]['title'].split(' ', 1)[0]
+                        
+                        if impact_value == "High":
+                            dict_["Time_Eastern"] = time_temp #Assign time value to dictionary
+                            dict_["Currency"] = item.find_all("td", {"class":"calendar__currency"})[0].text.strip() #Currency
+                            dict_["Impact"] = impact_value
+                            event = item.find_all("td",{"class":"calendar__event"})[0].text.strip() #Event Name
+                            dict_["Event"] = event
+                            ecoday.append(dict_)
 
         ecoDict = []
         for item in ecoday:
@@ -95,24 +94,28 @@ class PyEcoCal:
         json_object = json.dumps(ecoDict, indent=3)  
         return json_object, ecoday
 
-eco = PyEcoCal()
-json_str, ecoday = eco.get_economic_calendar("calendar?day=mar11.2023")
-eco_elements = json.loads(json_str)
-print(json_str)
-
 
 def run_discord_bot():
     intents = discord.Intents.default()
     client = discord.Client(intents=intents)
-    TOKEN = ''
+    TOKEN = 'YOUR_TOKEN_HERE'
 
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
+    # specify the time to send the embed every day (in 24-hour format)
+    et_tz = pytz.timezone('US/Eastern')
 
+    # Define the Amsterdam Timezone
+    am_tz = pytz.timezone('Europe/Amsterdam')
+    send_time = datetime.time(hour=6, minute=0)
+
+    async def send_embed():
+        
+        eco = PyEcoCal()
+        json_str, ecoday = eco.get_economic_calendar("calendar?day=mar16.2023")
+        eco_elements = json.loads(json_str)
+        print(json_str)
         # create an empty dictionary to group currencies by time
         currencies_by_time = {}
-
+     
         # group currencies by their time
         for eco_element in eco_elements:
             time = eco_element['time_eastern']
@@ -120,6 +123,42 @@ def run_discord_bot():
                 currencies_by_time[time].append(eco_element)
             else:
                 currencies_by_time[time] = [eco_element]
+        class MyView(discord.ui.View):
+            def __init__(self):
+                super().__init__()
+                self.pressed_users = set()
+            @discord.ui.button(label="", style=discord.ButtonStyle.success, emoji="🔔")
+            async def button_callback(self, button, interaction):
+                user_id = button.user.id
+                
+                if user_id not in self.pressed_users:
+                    self.pressed_users.add(user_id)
+                    await button.response.send_message("Je krijgt nu notificaties als er een event binnen 15 minuten begint!", ephemeral=True, delete_after=5.0)
+                else:
+                    self.pressed_users.discard(user_id)  # remove the user from the set
+                    await button.response.send_message("Je krijgt nu geen notificaties meer!", ephemeral=True, delete_after=5.0)
+
+
+        
+        for eco_element in eco_elements:
+            time_str = eco_element['time_eastern']
+
+            if time_str.lower() == 'all day':
+                continue
+            
+            time_obj = datetime.datetime.strptime(time_str, '%I:%M%p')
+
+            # Convert the Eastern Time to UTC
+            time_et = et_tz.localize(time_obj)
+            time_utc = time_et.astimezone(datetime.timezone.utc)
+
+            # Convert the UTC time to Amsterdam time
+            time_am = time_utc.astimezone(am_tz)
+            print("Eastern time:", time_et.strftime('%I:%M %p'))
+
+            # Print the Amsterdam time
+            print("amsterdam time:", time_am.strftime('%I:%M %p'))
+
 
         # create embed and add fields for each group of currencies with the same time
         embed = discord.Embed(color=15548997)
@@ -132,21 +171,42 @@ def run_discord_bot():
         log = client.get_channel(1051535739919290450)
 
         for time, currencies in currencies_by_time.items():
-                if len(currencies) == 1:
-                    # if there's only one currency for this time, display the currency in bold
-                    value = f"**{currencies[0]['currency']}**: {currencies[0]['event']}"
-                    if value:
-                        embed.add_field(name=f"\nTime: {time}", value=value, inline=False)
-                else:
-                    # if there are multiple currencies for this time, don't display the time label and make all currencies bold
-                    value = ''.join([f"**{currency['currency']}**: {currency['event']}\n" for currency in currencies])
-                    if value:
-                        embed.add_field(name='\u200b', value=value, inline=False)
+            if len(currencies) == 1:
+                # if there's only one currency for this time, display the time label
+                value = f"{currencies[0]['currency']}: {currencies[0]['event']}"
+                embed.add_field(name=f'Time: {time}', value=value, inline=False)
+            else:
+                # if there are multiple currencies for this time, don't display the time label
+                value = '\n'.join([f"{currency['currency']}: {currency['event']}" for currency in currencies])
+                embed.add_field(name=f'Time: {time}', value=value, inline=False)
 
         if len(embed.fields) > 0:
-                await log.send(embed=embed)
+            await log.send(embed=embed, view=MyView())
         else:
-                await log.send(embed=embed2)
-                print('No fields to send in the embed')
+            await log.send(embed=embed2, view=MyView())
+            print('No fields to send in the embed')
+
+    async def check_time():
+        # now = datetime.datetime.now()
+        # target_time = datetime.datetime.combine(now.date(), send_time)
+        # if now.time() >= send_time:
+        #     # if the current time is already past the target time, schedule the first message for tomorrow
+        #     target_time += datetime.timedelta(days=1)
+        # delay_seconds = (target_time - now).total_seconds()
+        # print(f"Waiting for {delay_seconds} seconds before sending the first message")
+        # await asyncio.sleep(delay_seconds)
+        while True:
+            await send_embed()
+            # wait for a day before checking the time again
+            await asyncio.sleep(86400)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="ForexFactory Calendar!"))
+        asyncio.create_task(check_time())
+
     client.run(TOKEN)
+
 run_discord_bot()
+
